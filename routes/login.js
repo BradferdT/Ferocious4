e17e47ac501f2bf53ec245afcfd127478ab4f511
Brad Transtrum <Bradf3rdcc@hotmail.com>
2017-04-24 15:47:45 -0700

    basic login page almost finished

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.render('index', { title: 'Testing'});
+  res.render('login/index', { title: 'ting'});
 })
 
 module.exports = router;
