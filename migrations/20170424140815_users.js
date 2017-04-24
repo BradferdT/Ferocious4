fbe5f7446189cbd28807d123f5834633095ac238
Brad Transtrum <Bradf3rdcc@hotmail.com>
2017-04-24 14:16:17 -0700

    Added users migration

--- migrations/20170424140815_users.js
+++ migrations/20170424140815_users.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(table){
+    table.increments();
+    table.string('name');
+    table.string('password');
+    table.string('email');
+    table.boolean('admin').defaultTo(false);
+    table.timestamps(true,true);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
