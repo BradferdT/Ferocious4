bbba07fb467a77187613005d2a700933a064a896
GJullian Flemister-King <gjullianfk@gmail.com>
2017-04-24 12:33:43 -0700

    created public directory and added static serve to server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 app.use('/', index);
