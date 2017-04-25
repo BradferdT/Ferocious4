f531154513a1ccc9293867d4f3541a3ab11422c5
Brad Transtrum <Bradf3rdcc@hotmail.com>
2017-04-25 11:08:20 -0700

    Added bcrypt using --save and added crypting to password for registering

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
+app.use(cookieParser("ferociousfour"));
 
 app.use('/', index);
 app.use('/login', require('./routes/login'));
