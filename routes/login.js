59f0ed89bbb682f04dd02f7b5e92729ef92344eb
Brad Transtrum <Bradf3rdcc@hotmail.com>
2017-04-25 13:18:42 -0700

    Login page works, crypts password and sets username and email as signed.

--- routes/login.js
+++ routes/login.js
@@ -7,15 +7,17 @@ router.get('/', function(req, res, next){
 })
 
 router.post('/signup', function(req, res, next){
-  if(req.body.email != null){
+  var username = req.body.username.toLowerCase();
+  if(req.body.email != ''){
     var crypted = bcrypt.hashSync(req.body.password, 8);
-    knex.raw('INSERT into users values (DEFAULT, ?, ?, ?)', [req.body.username, crypted, req.body.email])
+    knex.raw('INSERT into users values (DEFAULT, ?, ?, ?)', [username, crypted, req.body.email])
     .then(function(){
+      res.cookie('username', username, {signed: true});
+      res.cookie('email', req.body.email, {signed: true});
       res.send('Created account')
     })
     .catch(function(err){
       var message = 'Error';
-      console.log(err.constraint == 'users_username_unique');
       if(err.constraint == 'users_username_unique'){
         message = 'Username is taken!';
       }else if(err.constraint == 'users_email_unique'){
@@ -24,7 +26,23 @@ router.post('/signup', function(req, res, next){
       res.render('login/', {msg: message});
     })
   }else{
-    
+    knex.raw('SELECT * FROM users WHERE username = ?', [username])
+    .then(function(found){
+      console.log(found.rows.length);
+      if (found.rows.length != 0){
+                found_user_password = found.rows[0].password;
+                found_user_email = found.rows[0].email;
+      if (bcrypt.compareSync(req.body.password, found_user_password)){
+                 res.cookie("username", req.body.username, {signed: true});
+                 res.cookie('email', found_user_email, {signed: true});
+                 res.redirect("/login");
+                } else {
+                 res.render('login/', {msg: 'Incorrect Username or Password'})
+               }
+             } else {
+               res.render('login/', {msg: 'Username not found'});
+             }
+    })
   }
 })
 module.exports = router;
