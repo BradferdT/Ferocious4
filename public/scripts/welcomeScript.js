82a57c129cde16880df08191cdbf940c3010e4a4
Brad Transtrum <Bradf3rdcc@hotmail.com>
2017-04-25 17:15:08 -0700

    more changes to welcome page

--- public/scripts/welcomeScript.js
+++ public/scripts/welcomeScript.js
@@ -1,1 +1,9 @@
 $(".userOptions").dropdown();
+$('.scrollspy').scrollSpy();
+
+var options = [
+     {selector: '#demo', offset: 200, callback: function(el) {
+       Materialize.showStaggeredList($('.demo_list'));
+     } }
+   ];
+  Materialize.scrollFire(options);
