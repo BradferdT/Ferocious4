b47d7d8ed0188c95465525342e3d58f2171704a2
J Sandate <mrharisu@gmail.com>
2017-04-26 08:56:39 -0700

    add editing route and view for cpanel

--- routes/cpanel.js
+++ routes/cpanel.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-/* GET home page. */
+/* GET All users. */
 router.get('/', function(req, res, next) {
   knex.raw('SELECT * from users').then(function(data) {
     console.log(data)
@@ -11,11 +11,30 @@ router.get('/', function(req, res, next) {
     })
 });
 
+/* GET a single user */
 router.get('/show/:name', function(req, res, next) {
   knex.raw(`SELECT users.id, users.username, lists.list_name FROM users JOIN lists ON users.id = lists.user_id WHERE users.username='${req.params.name}'`).then(function(data) {
     res.render('cpanel/show', { title: 'user',
                                 user: data.rows[0]});
   })
+});
+
+/* UPDATE a user */
+router.get('/edit/:id', function(req, res, next) {
+   knex.raw(`SELECT * from users WHERE id=${req.params.id}`)
+       .then(function(data){
+         res.render('cpanel/edit', { userEdit: data.rows[0]});
+       })
+});
+
+router.post('/:id', function(req, res, next) {
+console.log(req.body.name);
+  knex.raw(`UPDATE users SET username = '${req.body.username}',
+                           password = '${req.body.password}',
+                           email = '${req.body.email}' WHERE id = ${req.body.id}`)
+      .then(function(){
+        res.redirect('/cpanel');
+      })
 });
 
 module.exports = router;
