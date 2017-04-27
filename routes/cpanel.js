598336a06bd6111feb3f06759569d887228a6234
J Sandate <mrharisu@gmail.com>
2017-04-27 13:34:35 -0700

    modified cpanel routes and index page (NOW FUNCTIONING)

--- routes/cpanel.js
+++ routes/cpanel.js
@@ -1,39 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var bcrypt = require('bcrypt');
 
 /* GET All users. */
 router.get('/', function(req, res, next) {
   knex.raw('SELECT * from users').then(function(data) {
-    console.log(data)
+
     res.render('cpanel/index', { title: 'admin_mode',
                                  users: data.rows});
     })
 });
 
-/* GET a single user */
-router.get('/show/:name', function(req, res, next) {
-  knex.raw(`SELECT users.id, users.username, lists.list_name FROM users JOIN lists ON users.id = lists.user_id WHERE users.username='${req.params.name}'`).then(function(data) {
-    res.render('cpanel/show', { title: 'user',
-                                user: data.rows[0]});
-  })
-});
-
 /* UPDATE a user */
-router.get('/edit/:id', function(req, res, next) {
-   knex.raw(`SELECT * from users WHERE id=${req.params.id}`)
-       .then(function(data){
-         console.log(data.rows[0])
-         res.render('cpanel/edit', { userEdit: data.rows[0]});
-       })
-});
 
+router.post('/update/:id', function(req, res, next) {
+var newUser = 'undefined';
+var newPass = 'undefined';
+var newEmail = 'undefined';
+var newPriv = 'undefined';
 
-router.post('/:id', function(req, res, next) {
-console.log(req.body.name);
-  knex.raw(`UPDATE users SET username = '${req.body.username}',
-                           password = '${req.body.password}',
-                           email = '${req.body.email}' WHERE id = ${req.body.id}`)
+if(req.body.username != '') {
+  newUser = knex.raw('UPDATE users SET username = ? WHERE id = ?',
+  [req.body.username, req.params.id])
+}
+if(req.body.password != '') {
+  var crypted = bcrypt.hashSync(req.body.password, 8);
+  newPass = knex.raw('UPDATE users SET password = ? WHERE id = ?',
+  [crypted, req.params.id]);
+}
+if(req.body.email != '') {
+  newEmail = knex.raw('UPDATE users SET email = ? WHERE id = ?',
+  [req.body.email, req.params.id])
+}
+if(req.body.admin != '') {
+  newPriv = knex.raw('UPDATE users SET admin = ? WHERE id = ?',
+  [req.body.admin, req.params.id])
+}
+
+Promise.all([newPass, newEmail, newPriv, newUser])
       .then(function(){
         res.redirect('/cpanel');
       })
