ab96a98939d55ae450ed59d8eb14b703853c468c
GJullian Flemister-King <gjullianfk@gmail.com>
2017-04-26 17:01:12 -0700

    add content to lists

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -19,12 +19,29 @@ $('#itemAddBtn').click(() => {
 });
 
 $('#walSearch').submit(() => {
-  $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=9nqubvqmveyf99yymdtaunax&query=apples`, (data) => {
+  const term = $('input[name=walmartSearch]').val();
+  $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=9nqubvqmveyf99yymdtaunax&query=${term}`, (data) => {
+    $('#searchList').empty();
     data.items.map((x,y) => {
       $('#searchList').append(`
         <li>
           <div class="collapsible-header">
-            ${JSON.stringify(x)}
+            <span style="text-align:initial;">
+              ${x.name}
+            </span>
+            <div>
+              <span>
+                <img src="${x.thumbnailImage}" alt="img"></img>
+                <h3 style="display:inline;">$${x.salePrice}</h3> at <a href="${x.productUrl}">WalMart</a>
+              </span>
+              <span style="float:right; margin-top:8vh;">
+                <i class="material-icons">view_stream</i>
+              </span>
+            </div>
+          </div>
+          <div class="collapsible-body">
+            <button type="" style="margin-right:2vh; margin-top:-3vh;" class="btn bttn" name="searchSubmit"><i class="material-icons">add</i>Add to List</button>
+            <button type="" style="margin-right:2vh; margin-top:-3vh;" class="btn bttn" name="searchSubmit">Similar Items</button>
           </div>
         </li>`)
     });
