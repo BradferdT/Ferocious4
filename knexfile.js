930189591b82ce32b7ebf05260bef6554a15679d
GJullian Flemister-King <gjullianfk@gmail.com>
2017-04-28 16:11:55 -0700

    Heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -3,20 +3,20 @@ module.exports = {
       client: 'pg',
       connection: 'postgres://localhost/list_app',
       migrations: {
-          directory: __dirname + '/db/migrations',
+          directory: __dirname + './db/migrations',
         },
       seeds: {
-          directory: __dirname + '/db/seeds',
+          directory: __dirname + './db/seeds',
         },
     },
   production: {
       client: 'pg',
       connection: process.env.DATABASE_URL,
       migrations: {
-          directory: __dirname + '/db/migrations',
+          directory: __dirname + './db/migrations',
         },
       seeds: {
-          directory: __dirname + '/db/seeds/production',
+          directory: __dirname + './db/seeds/production',
         },
     },
 };
